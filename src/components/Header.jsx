be548c6dfc2e0cf168c26349b0ff0468358e3655
Ishak Imanuel <frontendisawesomee@gmail.com>
2017-09-23 11:02:31 +0700

    add burger nav on phone view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Logo from '../img/logo.png';
 import Search from '../img/icon-search.png';
 
-const Header = () => {
-  return (
-    <header>
-      <div className="container grid">
-        <div className="logo">
-          <img src={Logo} alt="Logo Front End" />
+class Header extends Component {
+  state = {
+    isPhone: true
+  };
+  render() {
+    return (
+      <header>
+        <div className="container grid">
+          <div className="logo">
+            <img src={Logo} alt="Logo Front End" />
+            <i
+              className={`fa ${this.state.isPhone ? 'fa-bars' : 'fa-close'}`}
+              onClick={() => this.setState({ isPhone: !this.state.isPhone })}
+            />
+          </div>
+
+          <nav className={`nav ${this.state.isPhone ? 'hide' : ''}`}>
+            <ul className="menu">
+              <li>
+                <a href="">Menu 1</a>
+              </li>
+              <li>
+                <a href="">Menu 2</a>
+              </li>
+              <li className="menu-3">
+                <a href="">Menu 3</a>
+                <ul className="submenu">
+                  <li>
+                    <a href="">Sub Menu 3 Number 1</a>
+                  </li>
+                  <li>
+                    <a href="">Sub Menu 3 Number 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="">Menu 4</a>
+              </li>
+              <li>
+                <a href="" className="search-icon">
+                  <img src={Search} alt="search" />
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
-        <nav className="nav">
-          <ul className="menu">
-            <li>
-              <a href="">Menu 1</a>
-            </li>
-            <li>
-              <a href="">Menu 2</a>
-            </li>
-            <li className="menu-3">
-              <a href="">Menu 3</a>
-              <ul className="submenu">
-                <li>
-                  <a href="">Sub Menu 3 Number 1</a>
-                </li>
-                <li>
-                  <a href="">Sub Menu 3 Number 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="">Menu 4</a>
-            </li>
-            <li>
-              <a href="" className="search-icon">
-                <img src={Search} alt="search" />
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
-};
+      </header>
+    );
+  }
+}
 
 export default Header;
